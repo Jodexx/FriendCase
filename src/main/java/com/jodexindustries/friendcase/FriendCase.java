ce17a2d4b04bd8a89b473e7e56e6503103393353
Jodexx <0984134q@gmail.com>
2024-04-23 21:56:43 +0300

    2.2.0.8 api adaptation

--- src/main/java/com/jodexindustries/friendcase/FriendCase.java
+++ src/main/java/com/jodexindustries/friendcase/FriendCase.java
@@ -1,19 +1,21 @@
 package com.jodexindustries.friendcase;
 
-import com.jodexindustries.donatecase.api.SubCommandManager;
+import com.jodexindustries.donatecase.api.CaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FriendCase extends JavaPlugin {
     public static FriendCase instance;
+    private CaseManager api;
     @Override
     public void onEnable() {
         instance = this;
+        api = new CaseManager(this);
         saveDefaultConfig();
-        SubCommandManager.registerSubCommand("gift", new FriendSubCommand());
+        api.getSubCommandManager().registerSubCommand("gift", new FriendSubCommand());
     }
 
     @Override
     public void onDisable() {
-        SubCommandManager.unregisterSubCommand("gift");
+        api.getSubCommandManager().unregisterSubCommand("gift");
     }
 }
