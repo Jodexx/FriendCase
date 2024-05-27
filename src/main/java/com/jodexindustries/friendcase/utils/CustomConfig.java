a097b31c305a9029fab507529b8ab6a2db457946
Jodexx <0984134q@gmail.com>
2024-05-27 15:11:42 +0300

    added custom config for compatibility

---
+++ src/main/java/com/jodexindustries/friendcase/utils/CustomConfig.java
@@ -0,0 +1,25 @@
+package com.jodexindustries.friendcase.utils;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+public class CustomConfig {
+    private File configFile;
+    private YamlConfiguration config;
+    private final File dataFolder;
+
+    public CustomConfig(File dataFolder) {
+        this.configFile = new File(dataFolder, "Config.yml");
+        this.config = YamlConfiguration.loadConfiguration(configFile);
+        this.dataFolder = dataFolder;
+    }
+    public void setup() {
+        configFile = new File(dataFolder, "Config.yml");
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+    public FileConfiguration getConfig() {
+        return config;
+    }
+}
