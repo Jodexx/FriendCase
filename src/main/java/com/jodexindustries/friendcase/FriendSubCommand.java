ef93127d9e44e19d0eab5f91ecc1971521679e8e
Jodexx <0984134q@gmail.com>
2024-08-08 19:45:49 +0300

    api 2.2.4.5 adaptation

--- src/main/java/com/jodexindustries/friendcase/FriendSubCommand.java
+++ src/main/java/com/jodexindustries/friendcase/FriendSubCommand.java
@@ -1,22 +1,21 @@
 package com.jodexindustries.friendcase;
 
 import com.jodexindustries.donatecase.api.Case;
-import com.jodexindustries.donatecase.api.data.SubCommand;
-import com.jodexindustries.donatecase.api.data.SubCommandType;
+import com.jodexindustries.donatecase.api.data.subcommand.SubCommandExecutor;
+import com.jodexindustries.donatecase.api.data.subcommand.SubCommandTabCompleter;
 import com.jodexindustries.friendcase.utils.Tools;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class FriendSubCommand implements SubCommand {
+import static com.jodexindustries.donatecase.tools.Tools.rc;
+
+public class FriendSubCommand implements SubCommandExecutor, SubCommandTabCompleter {
     private final Tools t;
 
     public FriendSubCommand(Tools t) {
@@ -24,14 +23,14 @@ public class FriendSubCommand implements SubCommand {
     }
 
     @Override
-    public void execute(CommandSender sender, String[] args) {
-        if(sender instanceof Player) {
+    public void execute(CommandSender sender, String label, String[] args) {
+        if (sender instanceof Player) {
             Player p = (Player) sender;
             if (args.length == 0) {
                 sendHelp(sender);
             } else {
                 if (args.length < 3) {
-                    if(args.length == 1) {
+                    if (args.length == 1) {
                         if (args[0].equalsIgnoreCase("reload")) {
                             if (sender.hasPermission("donatecase.admin")) {
                                 t.getConfig().reloadConfig();
@@ -49,13 +48,13 @@ public class FriendSubCommand implements SubCommand {
                     try {
                         keys = Math.abs(Integer.parseInt(args[2]));
                     } catch (NumberFormatException e) {
-                                sender.sendMessage(rc(t.getConfig().getConfig().getString("Messages.NumberFormat")));
+                        sender.sendMessage(rc(t.getConfig().getConfig().getString("Messages.NumberFormat")));
                         return;
                     }
-                    if(Case.hasCaseByType(caseType)) {
-                        if(Case.getKeys(caseType, p.getName()) >= 1 && Case.getKeys(caseType, p.getName()) >= keys) {
-                            if(target != null) {
-                                if(target != p) {
+                    if (Case.hasCaseByType(caseType)) {
+                        if (Case.getKeys(caseType, p.getName()) >= 1 && Case.getKeys(caseType, p.getName()) >= keys) {
+                            if (target != null) {
+                                if (target != p) {
                                     Case.removeKeys(caseType, p.getName(), keys);
                                     Case.addKeys(caseType, target.getName(), keys);
                                     target.sendMessage(rc(
@@ -74,7 +73,7 @@ public class FriendSubCommand implements SubCommand {
                                     ));
                                 } else {
                                     sender.sendMessage(rc(
-                                           t.getConfig().getConfig().getString("Messages.GiftYourself", "")));
+                                            t.getConfig().getConfig().getString("Messages.GiftYourself", "")));
                                 }
                             } else {
                                 sender.sendMessage(rc(
@@ -83,7 +82,7 @@ public class FriendSubCommand implements SubCommand {
                         } else {
                             sender.sendMessage(rc(
                                     t.getConfig().getConfig().getString("Messages.MinNumber", "")
-                                    .replace("%required%", keys + "")
+                                            .replace("%required%", keys + "")
                             ));
                         }
                     } else {
@@ -95,69 +94,25 @@ public class FriendSubCommand implements SubCommand {
             sender.sendMessage(rc(t.getConfig().getConfig().getString("Messages.OnlyPlayers")));
         }
     }
+
     private void sendHelp(CommandSender sender) {
         for (String msg : t.getConfig().getConfig().getStringList("Messages.Help")) {
             sender.sendMessage(rc(msg));
         }
     }
-    private String rc(String text) {
-        return hex(text);
-    }
-
-    public static String hex(String text) {
-        return ChatColor.translateAlternateColorCodes('&', translateHexColorCodes(text));
-    }
-
-    public static final Pattern HEX_PATTERN = Pattern.compile("#([A-Fa-f0-9]{6})");
-    public static final char COLOR_CHAR = ChatColor.COLOR_CHAR;
-
-    public static String translateHexColorCodes(String message) {
-        Matcher matcher = HEX_PATTERN.matcher(message);
-        StringBuilder buffer = new StringBuilder(message.length() + 4 * 8);
-        while (matcher.find()) {
-            String group = matcher.group(1);
-            matcher.appendReplacement(buffer, COLOR_CHAR + "x"
-                    + COLOR_CHAR + group.charAt(0) + COLOR_CHAR + group.charAt(1)
-                    + COLOR_CHAR + group.charAt(2) + COLOR_CHAR + group.charAt(3)
-                    + COLOR_CHAR + group.charAt(4) + COLOR_CHAR + group.charAt(5)
-            );
-        }
-        return matcher.appendTail(buffer).toString();
-    }
 
     @Override
-    public List<String> getTabCompletions(CommandSender sender, String[] args) {
+    public List<String> getTabCompletions(CommandSender sender, String label, String[] args) {
         List<String> strings;
-        if(args.length == 1) {
+        if (args.length == 1) {
             strings = Bukkit.getOnlinePlayers().stream().map(HumanEntity::getName).collect(Collectors.toList());
             return strings;
         } else {
-            if(args.length == 2) {
-                strings = new ArrayList<>(Case.getCasesConfig().getCases().keySet());
+            if (args.length == 2) {
+                strings = new ArrayList<>(Case.getConfig().getCasesConfig().getCases().keySet());
                 return strings;
             }
         }
         return new ArrayList<>();
-    }
-
-    @Override
-    public SubCommandType getType() {
-        return SubCommandType.PLAYER;
-    }
-
-    @Override
-    public String[] getArgs() {
-        List<String> argsList = new ArrayList<>();
-
-        for (String args : t.getConfig().getConfig().getStringList("Api.Args")) {
-            argsList.add(rc(args));
-        }
-
-        return argsList.toArray(new String[0]);
-    }
-
-    @Override
-    public String getDescription() {
-        return rc(t.getConfig().getConfig().getString("Api.Description"));
     }
 }
--- src/main/java/com/jodexindustries/friendcase/utils/Tools.java
+++ src/main/java/com/jodexindustries/friendcase/utils/Tools.java
@@ -1,8 +1,15 @@
 package com.jodexindustries.friendcase.utils;
 
+import com.jodexindustries.donatecase.api.data.SubCommandType;
+import com.jodexindustries.donatecase.api.data.subcommand.SubCommand;
 import com.jodexindustries.friendcase.FriendSubCommand;
 import com.jodexindustries.friendcase.bootstrap.Main;
 import com.jodexindustries.friendcase.config.Config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jodexindustries.donatecase.tools.Tools.rc;
 
 public class Tools {
     private final Main main;
@@ -14,8 +21,25 @@ public class Tools {
     }
 
     public void load() {
-        main.getCaseAPI().getSubCommandManager().registerSubCommand("gift",
-                new FriendSubCommand(this));
+        FriendSubCommand friendSubCommand = new FriendSubCommand(this);
+
+        List<String> argsList = new ArrayList<>();
+
+        for (String args : getConfig().getConfig().getStringList("Api.Args")) {
+            argsList.add(rc(args));
+        }
+
+        String[] args = argsList.toArray(new String[0]);
+
+        SubCommand subCommand = main.getCaseAPI().getSubCommandManager().builder("gift")
+                .tabCompleter(friendSubCommand)
+                .executor(friendSubCommand)
+                .args(args)
+                .type(SubCommandType.PLAYER)
+                .description(rc(getConfig().getConfig().getString("Api.Description")))
+                .build();
+
+        main.getCaseAPI().getSubCommandManager().registerSubCommand(subCommand);
     }
 
     public void unload() {
